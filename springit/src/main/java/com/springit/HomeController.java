176ac6ee96ca7690fc958c2e53cfffabb7ca6519
mugunga <mugunga6@gmail.com>
2019-09-29 10:52:06 +0900

    testing

---
+++ springit/src/main/java/com/springit/HomeController.java
@@ -0,0 +1,13 @@
+package com.springit;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@GetMapping("/")
+	public String hello() {
+		return "What up";
+	}
+}
--- springit/src/main/java/com/springit/SpringitApplication.java
+++ springit/src/main/java/com/springit/SpringitApplication.java
@@ -1,16 +1,25 @@
 package com.springit;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
 public class SpringitApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringitApplication.class, args);
-		System.out.println("Hello");
-		
 	
+	}
+	
+	@Bean
+	@Profile("dev") 
+	CommandLineRunner runner() {
+		return args->{
+			System.out.println("Inside my Runner");
+		};
 	}
 
 }
